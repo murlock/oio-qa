3b01e20736b33663b47e27eef08a0242d941b5b8
Michael Bonfils <murlock42@gmail.com>
2017-10-17 14:37:47 +0200

    Enable oio-fs test

---
+++ ci/run/run_oiofs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# we suppose that a docker image with name `sds-source` already exists
+
+# if set, it assume that a docker image is already running
+SKIP_DOCKER=$1
+DOCKER_TAG=latest
+OUTPUT_RESULT=$HOME/output
+
+OUTPUT_CUR_TEST=
+set -e
+set -x
+
+function init() {
+    local TEST=$1
+
+    OUTPUT_CUR_TEST=${OUTPUT_RESULT}/${TEST}
+    mkdir -p ${OUTPUT_CUR_TEST}
+}
+
+function run_oiofs_pjfs() {
+    init oiofs_pjfs
+    $HOME/oio-fs/docker/run.sh
+}
+
+
+run_oiofs_pjfs
